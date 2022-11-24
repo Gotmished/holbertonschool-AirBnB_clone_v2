65bef46f29b49eee9089be52a4139c96ee6a9062
Gotmished <5002@holbertonschool.com>
2022-11-24 03:29:27 -0800

    Fixing small typographical errors

--- models/place.py
+++ models/place.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey=("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey=("users.id"), nullable=False)
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, default=0, nullable=False)
