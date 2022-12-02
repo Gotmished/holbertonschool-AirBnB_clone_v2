7aec42dcd5171b3307a77b1e994a8464821508d9
Gotmished <5002@holbertonstudents.com>
2022-12-02 08:13:20 +0000

    Initial commit task eight - displaying cities by state, and using cities table relationship

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A script to start a Flask web application that fetches data
+from storage, removes the current session after each request,
+and displays an HTML page listing all states present
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__, template_folder='templates')
+
+@app.teardown_appcontext
+def app_teardown(exception=None):
+    """Removes current session following each request"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def show_cities_by_state():
+    """
+    Displays all cities currently in storage
+    in a rendered HTML page, and arranged by state
+    """
+    return render_template('8-cities_by_states.html',
+                           state_dict=storage.all("State"))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
